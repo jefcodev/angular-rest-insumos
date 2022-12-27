6a5a8eedcd7b3ab93cf13cca6bba4bba1b94ff5e
Jefferson Pinchao <jeffersonpinchao@hotmail.com>
2022-12-27 18:06:43 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  public loginForm!: FormGroup
+
+  constructor(private formbuilder: FormBuilder,private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formbuilder.group({
+      nombre_usuario: [''],
+      clave_usuario: ['', Validators.required],
+      tipo_usuario: ['']
+    })
+  }
+  login(){
+    this.http.get<any>("http://localhost:4000/usuarios")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        return a.nombre_usuario === this.loginForm.value.nombre_usuario && a.clave_usuario === this.loginForm.value.clave_usuario 
+      });
+      
+      if(user){
+      this.router.navigate(["home"])
+      }else{
+        alert("user not found")
+      }
+    },err=>{
+      alert("Something went wrong")
+    })
+  }
+}
