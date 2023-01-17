7218e415756b15a9eddc1ae8483f002b5120ef75
Quinatoa <paul1234alexander@gmail.com>
2023-01-17 09:14:55 -0500

    get reciclados

--- src/app/components/recicladas/recicladas.component.ts
+++ src/app/components/recicladas/recicladas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModelRecicladasI } from '../../modelos/modelo.recicladas';
+import { RecicladasService } from '../../servicios/recicladas/recicladas.service';
 
 @Component({
   selector: 'app-recicladas',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recicladas.component.scss']
 })
 export class RecicladasComponent implements OnInit {
+  recicladas: ModelRecicladasI[] = [];
 
-  constructor() { }
+  constructor(private recicladasServices: RecicladasService) { }
 
   ngOnInit(): void {
+    this.showAllRecicladas()
   }
-
+  showAllRecicladas() {
+    this.recicladasServices.getAllRecicladas().subscribe(
+      (recicladas: any) => {
+        this.recicladas = recicladas
+        console.log(this.recicladas)
+      },
+      (error) => console.log(error)
+    );
+  }
 }
--- src/app/servicios/recicladas/recicladas.service.ts
+++ src/app/servicios/recicladas/recicladas.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecicladasService {
 
-  constructor() { }
+  url: string = 'https://app-sistemas-inventarios.herokuapp.com/';
+  // constructor(private http:HttpClient) { }
+
+  // url:string='http://localhost:4000/';
+  constructor(private http: HttpClient) { }
+
+  getAllRecicladas() {
+    let direccion = this.url + "recicladas";
+    return this.http.get(direccion);
+  }
+  saveRecicladas(form: any) {
+    let direccion = this.url + "recicladas";
+    return this.http.post(direccion, form);
+  }
+  updateRecicladas
+  (form: any) {
+    let direccion = this.url + "recicladas";
+    return this.http.put(direccion, form);
+  }
 }
