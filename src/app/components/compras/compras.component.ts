14adf7ebd4d25d3f209cd92b607d5fb1490cb7b5
Quinatoa <paul1234alexander@gmail.com>
2023-01-17 07:55:14 -0500

    get compras

--- src/app/components/compras/compras.component.ts
+++ src/app/components/compras/compras.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModelComprasI } from 'src/app/modelos/modelo.compras';
+import { ComprasService } from 'src/app/servicios/compras/compras.service';
 
 @Component({
   selector: 'app-compras',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compras.component.scss']
 })
 export class ComprasComponent implements OnInit {
+  compras:ModelComprasI[] = [];
 
-  constructor() { }
+  constructor( private comprasServices:ComprasService) { }
 
   ngOnInit(): void {
+    this.showAllCompras()
+  }
+  showAllCompras() {
+    this.comprasServices.getAllCompras().subscribe(
+      (compras: any) => {
+        this.compras = compras
+        console.log(this.compras)
+      },
+      (error) => console.log(error)
+    );
   }
 
 }
--- src/app/servicios/compras/compras.service.ts
+++ src/app/servicios/compras/compras.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComprasService {
 
-  constructor() { }
+  url: string = 'https://app-sistemas-inventarios.herokuapp.com/';
+  // constructor(private http:HttpClient) { }
+
+  // url:string='http://localhost:4000/';
+  constructor(private http: HttpClient) { }
+
+  getAllCompras() {
+    let direccion = this.url + "compras";
+    return this.http.get(direccion);
+  }
+  saveCompras(form: any) {
+    let direccion = this.url + "compras";
+    return this.http.post(direccion, form);
+  }
+  updateCompras(form: any) {
+    let direccion = this.url + "compras";
+    return this.http.put(direccion, form);
+  }
 }
